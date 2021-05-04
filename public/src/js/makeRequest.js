06cf51532204f64e7a8630fbaadee0e23fe1adc8
micaiah-effiong <micaiah.effiong@gmail.com>
2021-05-04 20:43:32 +0100

    refactor to use async/await

--- public/src/js/makeRequest.js
+++ public/src/js/makeRequest.js
@@ -1,7 +1,9 @@
-function makeRequest(url, method, options) {
-  return fetch(url, {
+export default async function makeRequest(url, method, options) {
+  const extra = {
     method: method,
     headers: options.headers,
     body: JSON.stringify(options.body),
-  }).then((res) => res.json());
+  };
+  const res = await fetch(url, extra);
+  return await res.json();
 }
